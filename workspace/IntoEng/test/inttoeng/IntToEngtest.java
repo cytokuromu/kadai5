f26b540218b635ca765b737b9ce87961ec5091be
g16904as <g16904as@gm.tsuda.ac.jp>
2018-05-18 14:01:49 +0900

    ドライバー：g16904 ナビゲーター:g16901 100まで出力可能

--- workspace/IntoEng/test/inttoeng/IntToEngtest.java
+++ workspace/IntoEng/test/inttoeng/IntToEngtest.java
@@ -9,38 +9,41 @@ public class IntToEngtest {
 
 	@Test
 	public void さんからthreeへ変更() {
-		IntToEng ite=new IntToEng();
-		String expected="three";
+		IntToEng ite = new IntToEng();
+		String expected = "three";
 		String actual = ite.translateEng(3);
-		assertThat(actual, is(expected)); 
+		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void じゅうはちからeighteenへ変更() {
-		IntToEng ite=new IntToEng();
-		String expected="eighteen";
+		IntToEng ite = new IntToEng();
+		String expected = "eighteen";
 		String actual = ite.translateEng(18);
-		assertThat(actual, is(expected)); 
+		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void きゅうじゅうよんからninety_fourへ変更() {
-		IntToEng ite=new IntToEng();
-		String expected="ninety four";
+		IntToEng ite = new IntToEng();
+		String expected = "ninety four";
 		String actual = ite.translateEng(94);
-		assertThat(actual, is(expected)); 
+		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void ななじゅうからseventyへ変更() {
-		IntToEng ite=new IntToEng();
-		String expected="seventy";
+		IntToEng ite = new IntToEng();
+		String expected = "seventy";
 		String actual = ite.translateEng(70);
-		assertThat(actual, is(expected)); 
+		assertThat(actual, is(expected));
 	}
 	
 	@Test
 	public void ひゃくからone_hundredへ変更() {
-		IntToEng ite=new IntToEng();
-		String expected="one hundred ";
+		IntToEng ite = new IntToEng();
+		String expected = "one hundred ";
 		String actual = ite.translateEng(100);
-		assertThat(actual, is(expected)); 
+		assertThat(actual, is(expected));
 	}
 }
